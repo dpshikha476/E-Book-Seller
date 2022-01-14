4ee4431f333165bf57f602addd99869f2ed8539f
Deepshikha <shikha8835@gmail.com>
2022-01-14 14:41:49 +0530

    Book Routes Created

--- routes/authors.js
+++ routes/authors.js
@@ -10,7 +10,7 @@ router.get('/', async(req,res) => {
     }
 
     try{
-        const authors = await Author.find({})
+        const authors = await Author.find(searchOptions)
         res.render('authors/index' ,{ 
             authors: authors, 
             searchOptions: req.query 
